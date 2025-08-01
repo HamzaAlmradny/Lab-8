93f9d993db540520b5883124186a2a0b91a6b32c
HamzaAlmradny <h.almradny@gmail.com>
2025-08-01 18:07:05 +0300

    Lab 8

--- Lab 8/Lab 8.cpp
+++ Lab 8/Lab 8.cpp
@@ -1,4 +1,21 @@
 
+// Documentation Section
+
+/*
+File Name : Lab 8
+Author : Hamza Alsayed Almradny
+Version : V.1
+Purpose : Ask the user to input the following 3 integer values: 6, 2, 5  and store each value in a different variable then Set the first bit in the first variable, Clear the second bit in the second variable, and then toggle the third bit in the third variable. Finally, print the result on CMD.
+Date : 17/7/2025
+*/
+
+
+
+
+
+
+
+
 #include <iostream>
 using namespace std;
 
